422bc0c118c11e9d280259f245f7e5c68405841d
Paulito Palmes <ppalmes@gmail.com>
2021-03-30 22:06:11 +0100

    transition to github actions

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,22 @@
 import PyCall: pyimport
 
-# See https://stackoverflow.com/questions/12332975/installing-python-module-within-code.
-const PIP_PACKAGES = ["lale"]
+function installpypackage()
+   # See https://stackoverflow.com/questions/12332975/installing-python-module-within-code.
+   PIP_PACKAGES = ["lale"]
+   try
+      pyimport("lale")
+      @info "lale succesfully installed"
+   catch
+      try
+         sys = pyimport("sys")
+         subprocess = pyimport("subprocess")
+         subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", "--upgrade", "--force-reinstall", PIP_PACKAGES...])
+         @info "lale succesfully installed"
+      catch
+         println("scikit-learn failed to install")
+      end
+   end
+end
+installpypackage()
 
-sys = pyimport("sys")
-subprocess = pyimport("subprocess")
-subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", "--upgrade", "--force-reinstall", PIP_PACKAGES...])
+
