1a84c6bf29693455f81e1984a4af278b38f43d3e
Paulito Palmes <ppalmes@gmail.com>
2021-03-31 01:53:31 +0100

    install lale during build if not windows os

--- deps/build.jl
+++ deps/build.jl
@@ -3,20 +3,20 @@ import PyCall: pyimport
 function installpypackage()
    # See https://stackoverflow.com/questions/12332975/installing-python-module-within-code.
    PIP_PACKAGES = ["lale"]
-   try
-      pyimport("lale")
-      @info "lale succesfully installed"
-   catch
+   if !Sys.iswindows()
       try
-         sys = pyimport("sys")
-         subprocess = pyimport("subprocess")
-         subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", "--upgrade", "--force-reinstall", PIP_PACKAGES...])
+         pyimport("lale")
          @info "lale succesfully installed"
       catch
-         println("scikit-learn failed to install")
+         try
+            sys = pyimport("sys")
+            subprocess = pyimport("subprocess")
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", "--upgrade", "--force-reinstall", PIP_PACKAGES...])
+            @info "lale succesfully installed"
+         catch
+            println("scikit-learn failed to install")
+         end
       end
    end
 end
 installpypackage()
-
-
