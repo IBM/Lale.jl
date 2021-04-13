d1a6b243fb399e641998c7519f6e7fda7cc078ce
Paulito Palmes <ppalmes@gmail.com>
2021-04-13 02:09:21 +0100

    key error test

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,6 @@ warnings.filterwarnings("ignore")
 # test modules
 include("test_sklearn.jl")
 include("test_autogen.jl")
-include("test_lale.jl")
 include("test_lalepreprocessing.jl")
 #include("test_laleoptimizer.jl")
 
